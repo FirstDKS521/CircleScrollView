e4f01a0b99a4ab78133f02df38688bc03b932b7e
duks <duks@robooot.com>
2016-08-12 17:52:35 +0800

    first commit

---
+++ CircleScrollView/DKSCircleImage.h
@@ -0,0 +1,13 @@
+//
+//  DKSCircleImage.h
+//  CircleScrollView
+//
+//  Created by aDu on 16/8/12.
+//  Copyright © 2016年 DuKaiShun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DKSCircleImage : UIView
+
+@end
---
+++ CircleScrollView/DKSCircleImage.m
@@ -0,0 +1,21 @@
+//
+//  DKSCircleImage.m
+//  CircleScrollView
+//
+//  Created by aDu on 16/8/12.
+//  Copyright © 2016年 DuKaiShun. All rights reserved.
+//
+
+#import "DKSCircleImage.h"
+
+@implementation DKSCircleImage
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
